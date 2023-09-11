a7ec593369deff6958d7307b88052e99b56d3b9e
Carlos1507 <ayala.carlos@pucp.edu.pe>
2023-09-11 15:18:47 -0500

    inicio

---
+++ proyecto.py
@@ -0,0 +1,8 @@
+
+
+
+if __name__ == "__main__":    
+    print("Welcome to the Cloud Service: CGC (The Cloud Gods Carry)")
+    print("Please enter your credentials to log in to the system: ")
+    name = input("")
+    #print("What would you like to do today?")
