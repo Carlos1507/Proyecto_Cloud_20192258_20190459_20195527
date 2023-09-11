e7eed52a185238899149ac8e267f66d441b5af64
Carlos1507 <ayala.carlos@pucp.edu.pe>
2023-09-11 16:24:49 -0500

    definiciones

--- proyecto.py
+++ proyecto.py
@@ -1,18 +1,25 @@
+# Definición de librerías y constantes
+
+# Definición de las clases a implementar (elementos):
 class virtualMachine:
     pass
 class slice:
     pass
 class user:
     pass
-def conexionBD():
-    pass
+
+# Definición de los módulos a implementar:
 def autorizacion():
+    pass
+
+# Definición de funciones adicionales a implementar:
+def conexionBD():
     pass
 def menu():
     opcion = ""
     print("What would you like to do today?")
     return opcion
-
+# Función principal
 if __name__ == "__main__":    
     print("Welcome to the Cloud Service: CGC (The Cloud Gods Carry)")
     print("Please enter your credentials to log in to the system: ")
