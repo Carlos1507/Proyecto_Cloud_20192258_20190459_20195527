d728f146e1d5fe0479f7da507b030f0b8016cb44
Carlos1507 <ayala.carlos@pucp.edu.pe>
2023-09-13 15:00:13 -0500

    interfaz grafica

--- proyecto.py
+++ proyecto.py
@@ -1,6 +1,5 @@
 # Definición de librerías y constantes
-from tkinter import *
-from tkinter import ttk 
+import tkinter as tk
 # Definición de las clases a implementar (elementos):
 class virtualMachine:
     pass
@@ -8,13 +7,6 @@ class slice:
     pass
 class user:
     pass
-
-root = Tk()
-frm = ttk.Frame(root, padding=10)
-frm.grid()
-frm.Label(frm, text="Welcome to the Cloud Service: CGC (The Cloud Gods Carry)\nPlease enter your credentials to log in to the system: ")
-frm.Button(frm,Text="Login", command=root.destroy).grid(column=1, row=0)
-root.mainloop()
 
 # Definición de los módulos a implementar:
 def autorizacion():
@@ -33,9 +25,32 @@ if __name__ == "__main__":
   #  print("Please enter your credentials to log in to the system: ")
   #  usuario = autorizacion()
   #  menu(usuario)
-    root = Tk()
-    frm = ttk.Frame(root, padding=10)
-    frm.grid()
-    ttk.Label(frm, text="Welcome to the Cloud Service: CGC (The Cloud Gods Carry)\nPlease enter your credentials to log in to the system: ").grid(column=0, row=0)
-    ttk.Button(frm, text="Login", command=root.destroy).grid(column=1, row=0)
+    root = tk.Tk()
+    root.title("Cloud Service")
+
+    # Mensajes iniciales
+    mensaje_inicial = tk.Label(root, text="Welcome to the Cloud Service: CGC (The Cloud Gods Carry)", background="#80FF80")
+    mensaje_inicial.grid(row=0, column=0, columnspan=2, padx=10, pady=10)
+
+    mensaje_inicial_2 = tk.Label(root, text="Please enter your credentials to log in to the system:")
+    mensaje_inicial_2.grid(row=1, column=0, columnspan=2, padx=10, pady=10)
+
+    # Usuario
+    user_label = tk.Label(root, text="User:")
+    user_label.grid(row=4, column=0, padx=10, pady=10, sticky=tk.E)
+
+    username = tk.Entry(root)
+    username.grid(row=4, column=1, padx=10, pady=10)
+
+    # Contraseña
+    password_label = tk.Label(root, text="Password:")
+    password_label.grid(row=5, column=0, padx=10, pady=10, sticky=tk.E)
+
+    password = tk.Entry(root, show="*")
+    password.grid(row=5, column=1, padx=10, pady=10)
+
+    # Botón de inicio de sesión
+    login_button = tk.Button(root, text="Login", command=autorizacion)
+    login_button.grid(row=6, column=0, columnspan=2, padx=10, pady=10)
+
     root.mainloop()
