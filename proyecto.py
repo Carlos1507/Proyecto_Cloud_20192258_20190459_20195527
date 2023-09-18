713bbe4b54dd74e0dda8ad9f6e42079748565589
Carlos1507 <ayala.carlos@pucp.edu.pe>
2023-09-17 21:29:31 -0500

    ui-avance1

--- proyecto.py
+++ proyecto.py
@@ -1,9 +1,10 @@
 # Definición de librerías y constantes
 import http.client as httplib
 import json
+import time
+import getpass
 
 headers = {'Content-type': 'application/json'}
-
 # Definición de las clases a implementar (elementos):
 class virtualMachine:
     pass
@@ -16,8 +17,8 @@ endpoint="http://127.0.0.1:3000/"
 class APIServ(object):
     def __init__(self, server):
         self.server = server
-    def postReq(self, data):
-        ret = self.rest_call('POST', "", data)
+    def postReq(self, subruta, data):
+        ret = self.rest_call('POST', subruta, data)
         return ret[0]==200
     def getReq(self, subruta):
         ret = self.rest_call('GET', subruta)
@@ -30,7 +31,6 @@ class APIServ(object):
             conn.request(action, path, body, headers)
         conn.request(action, path, "", headers)
         response = conn.getresponse()
-    
         ret = (response.status, response.reason, response.read())
         print(ret)
         conn.close()
@@ -39,23 +39,72 @@ class APIServ(object):
 # Definición de los módulos a implementar:
 def autorizacion():
     user = input("Usuario: ")
-    passw = input("Contraseña: ")
-    print(f"Usuario: {user} Contraseña: {passw}")
+    passw = ""
+    try:
+        passw = getpass.getpass("Contraseña: ")
+    except Exception as err:
+        print("Error: " + err)    
     print("Validando...")
-    resultado = pusher.getReq("imprimir")
-    
+    time.sleep(2)
+  #  resultado = pusher.getReq("imprimir")
+  #  valido = pusher.postReq("validarPOST", {"user": user, "password": passw})
+    if(user == "diego" and passw =="root"):
+        return True
+    else:
+        print("Credenciales incorrectas")
+        autorizacion()
+# Funciones del menú
+# Crear Slice
+def topologiaPredeterminada():
+    print("Seleccione el tipo de topología: \n\t1. Malla\n\t2. Árbol\n\t3. Anillo\n\t4. Lineal")
+def topologiaPersonalizada():
+    pass
+def crearSlice():
+    print("Tipo de topología:\n\t1. Predeterminado\n\t2. Personalizado")
+    topologia = input("Opción: ")
+    if(topologia == "1"):
+        topologiaPredeterminada()
+    elif(topologia == "2"):
+        topologiaPersonalizada()
+    else:
+        crearSlice()
+# Listar Slice
+def listarSlice():
+    pass
+# Definir zonas de disponibilidad
+def zonasDisponibilidad():
+    pass
 
 # Definición de funciones adicionales a implementar:
-def conexionBD():
-    pass
 def menu():
     opcion = ""
     print("What would you like to do today?")
-    return opcion
+    print("1. Crear Slice")
+    print("2. Listar Slices")
+    print("3. Definir zona de disponibilidad")
+    opcion = input("Seleccione una opción: ")
+    if(opcion =="1"):
+        crearSlice()
+    elif(opcion =="2"):
+        listarSlice()
+    elif(opcion =="3"):
+        zonasDisponibilidad()        
+    else:
+        menu()
+def seleccionarPlataforma():
+    print("Select the platform: ")
+    print("1. Linux")
+    print("2. OpenStack")
+    opcionPlataforma = input("Option: ")
+    if(opcionPlataforma =="1" or opcionPlataforma=="2"):
+        return opcionPlataforma
+    else:
+        seleccionarPlataforma()
 # Función principal
 if __name__ == "__main__":    
     pusher = APIServ("127.0.0.1")
-    print("Welcome to the Cloud Service: CGC (The Cloud Gods Carry)")
+    print("Welcome to the Cloud Service: CCG (The Cloud Computing Gods)")
     print("Please enter your credentials to log in to the system: ")
-    usuario = autorizacion()
-   # menu(usuario)
+    valido = autorizacion()
+    opcionPlataforma = seleccionarPlataforma()
+    opcion = menu()
--- servidor.js
+++ servidor.js
@@ -1,5 +1,6 @@
 var mysql = require('mysql2');
 const tls = require('node:tls');
+const bodyParser = require("body-parser");
 const express = require('express');
 var app = express();
 
@@ -14,7 +15,7 @@ con.connect(function(err) {
     if (err) throw err;
     con.query("SELECT * FROM persona", function(err,result, fields){
         if(err) throw err;
-        console.log(result);
+       // console.log(result);
     });
 });
 
@@ -27,5 +28,32 @@ app.get("/imprimir", (req, res) => {
         res.send(result);
     });
 })
+
+app.get("/validar/:user/:password", (req, res) => {
+    let user = req.params.user;
+    let password = req.params.password;
+    let sql = "SELECT * FROM happyhierba.persona where (correo= ? and contrasenia= ?)"
+    let params = [user, password];
+    con.query(sql, params, function (err, result) {
+        if(err) throw err;
+        res.send(result);
+    })
+});
+
+// usar bodyParser.json() cuando se manda como raw -> json
+// usar bodyParser.urlencoded({extended: true}) cuando se manda como x-www-form-unlencoded
+
+app.post("/validarPOST", bodyParser.json(), function (req, res) {
+    let user = req.body.user;
+  
+    let password = req.body.password;
+    let sql = "SELECT * FROM happyhierba.persona where (correo= ? and contrasenia= ?)"
+    let params = [user, password];
+    con.query(sql, params, function (err, result) {
+        if(err) throw err;
+        res.send(result);
+        console.log(result);
+    })
+});
 
 app.listen(port)
