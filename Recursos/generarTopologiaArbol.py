799f09358cccef0ecd4ca10acb6e69eb801b5bf1
Carlos1507 <ayala.carlos@pucp.edu.pe>
2023-11-03 18:28:03 -0500

    a

--- Recursos/generarTopologiaArbol.py
+++ Recursos/generarTopologiaArbol.py
@@ -1,46 +1,42 @@
 from Recursos.funcionGestionTopologias import graficarTopologia
-links = []
-global nodos
-nodos = []
-global numHijos
-nodoInicial = 'sw1'
-nodos = [nodoInicial]
-links = []
-contador = 1
-contador_vms = 0
 
-def gener_hijos(parent, numHijos):
-    global nodos, contador
+def gener_hijos(parent, numHijos, links, nodos, contador):
     for _ in range(numHijos):
         contador += 1
         hijo = f'sw{contador}'
         nodos.append(hijo)
         enlace = (parent, hijo)
         links.append(enlace)
+    return links, nodos, contador
 
-def gener_vms(parent, numHijos):
-    global nodos, contador_vms
+def gener_vms(parent, numHijos, links, nodos, contador_vms):
     for _ in range(numHijos):
         contador_vms += 1
         hijo = f'vm{contador_vms}'
         nodos.append(hijo)
         enlace = (parent, hijo)
         links.append(enlace)
+    return links, nodos, contador_vms
 
 def generarArbol(numHijos, nivel):  
+    nodoInicial = 'sw1'
+    nodos = [nodoInicial]
+    links = []
+    contador = 1
+    contador_vms = 0
     for i in range(1, nivel + 1):
         num_enlaces = numHijos ** i
         nodos_elegir = int(num_enlaces / numHijos)
         if i == 1:
-            nodos_elegidos = [nodos[-1]]
+            nodos_elegidos = [nodoInicial]
         else:
             nodos_elegidos = nodos[-nodos_elegir:]
         if(i==nivel):
             for nodo in nodos_elegidos:
-                gener_vms(nodo, numHijos)
+                links, nodos, contador_vms = gener_vms(nodo, numHijos, links, nodos, contador_vms)
         else:
             for nodo in nodos_elegidos:
-                gener_hijos(nodo, numHijos)
+                links, nodos, contador = gener_hijos(nodo, numHijos, links, nodos, contador)
     VMS = numHijos**nivel
     listaSWs = nodos[:-VMS]
     listaVMs = nodos[VMS-1:]
